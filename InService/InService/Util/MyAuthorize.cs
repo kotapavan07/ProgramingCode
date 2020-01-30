ad344c46d55dd1fa29e734e6c5e9bfbd85c4e15e
unknown <kotapavankumar07@gmail.com>
2020-01-30 12:32:07 +0530

    he

---
+++ InService/InService/Util/MyAuthorize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace InService.Util
+{
+    public class MyAuthorize : AuthorizeAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+        }
+    }
+}
